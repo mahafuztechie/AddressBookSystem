9bb80e12cbcb04bf5b074b3676e94abeb847011b
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-20 16:32:56 +0530

    [mahafuz] adding uc2_addContact

---
+++ AddressBookSystem/AddressBookSystem/AddressBookDetails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBookDetails
+    {
+
+        ArrayList allcontacts = new ArrayList();
+       
+        //method to add Contacts
+        public void AddContacts()
+        {
+            Console.WriteLine("Enter First Name : ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter City : ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Address : ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zipcode : ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number : ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Contacts contact = new Contacts(firstName, lastName, city, state, address, email, zip, phoneNumber);
+            Console.WriteLine("contact added: " +contact);
+
+            //adding contact into list
+            allcontacts.Add(contact);  
+        }
+
+        // method to display contact
+     public void display()
+        {
+            //foreach loop to iterate all contacts from list & print
+            foreach(Contacts contact in allcontacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+
+  
+}
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookSystem;
+using System.Collections;
 Console.WriteLine("Welcome to Adddress book system");
+int num;
+string key = "z";
+
+
+AddressBookDetails addressbook  = new AddressBookDetails();
+
+/// <summary>
+/// implementing a whileLoop with switch case to ask user if he wants to add or display contact
+/// </summary>
+while (key != "n")
+{
+    Console.WriteLine("Select an Option");
+    Console.WriteLine("1 - Addcontact, 2 - Display contact, 3 - deleteContact");
+    num = Convert.ToInt32(Console.ReadLine());
+    switch (num)
+    {
+        case 1:
+            addressbook.AddContacts();
+            break;
+        case 2:
+            addressbook.display();
+            break;
+    }
+    Console.WriteLine("Do u want to continue? Press y/n");
+   
+    key = Console.ReadLine();
+}
+Console.ReadLine();
