5ca229552ed3638d490217db48deee27b9444725
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-23 19:45:53 +0530

    [mahafuz] adding uc22 read from json server

---
+++ addressbookRestSharp/AddressBookRestSharpTestCases.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace addressbookRestSharp
+{
+    public class Contacts
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+    [TestClass]
+    public class AddressBookRestSharpTestCases
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:3000");
+        }
+
+        private RestResponse GetAddressList()
+        {
+            // arrange
+            RestRequest request = new RestRequest("/Contacts", Method.Get);
+
+            // act
+            RestResponse response = client.ExecuteAsync(request).Result;
+            return response;
+        }
+
+        [TestMethod]
+        public void OnCallingGETApi_ReturnAddressList()
+        {
+            RestResponse response = GetAddressList();
+
+            // assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Contacts> dataResponse = JsonConvert.DeserializeObject<List<Contacts>>(response.Content);
+            Assert.AreEqual(1, dataResponse.Count);
+        }
+    }
+}
---
+++ addressbookRestSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbookRestSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbookRestSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4de89077-a5f1-47bd-bdd7-be27fc65389f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
